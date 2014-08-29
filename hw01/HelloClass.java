96cbba6bdb5feb349bc3a54e00847b82e35e2689
ryanstaffen <rns216@lehigh.edu>
2014-08-29 00:23:48 +0000

    updated HelloClass

---
+++ hw01/HelloClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//Ryan Staffen
+//CSE 002 Sec. 111
+//8/28/2014
+//
+// first compile the program
+//      javac HelloClass.java
+// run the program
+//      java HelloClass//
+
+//  define a class
+public class HelloClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the welcome statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    
+//  print out the Lehigh Network ID
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-R--N--S--2--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    
+//  print autobiographical statement
+    System.out.println("I am a junior Industrial and Systems Engineering major on the Information Technology Track. This is my first experience with Java programming."  );
+  }
+} 
