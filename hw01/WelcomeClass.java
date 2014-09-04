411a31c3ab6f6bb35298ac63817f9ed50fe7b569
ryanstaffen <rns216@lehigh.edu>
2014-09-04 00:26:18 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,32 @@
+///////////////////////////////////////////////////////////
+//Ryan Staffen
+//CSE 002 Sec. 111
+//8/28/2014
+//
+// first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java WelcomeClass//
+
+//  define a class
+public class WelcomeClass{
+    
+//  add main method
+    public static void main(String[] args) {
+
+//  print the welcome statement
+    System.out.println("  -----------");
+    System.out.println("  | WELCOME |");
+    System.out.println("  -----------");
+    
+//  print out the Lehigh Network ID
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("|-R--N--S--2--1--6-|");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    
+//  print autobiographical statement
+    System.out.println("I am a junior Industrial and Systems Engineering major on the Information Technology Track. This is my first experience with Java programming."  );
+  }
+} 
