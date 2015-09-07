8699d60e295d8311fd78cdd0c0579fc1d59ebbfc
ryanstaffen <rns216@lehigh.edu>
2015-09-07 19:06:23 +0000

    updated Arithmetic

--- hw02/Arithmetic.java
+++ hw02/Arithmetic.java
@@ -1,7 +1,7 @@
 //Ryan Staffen
-//CSE 002 Section 111
+//CSE 002 Section 315
 //HW02
-//9-9-2014
+//9-7-2015
 
 // first compile the program 
 //      javac Arithmetic.java
@@ -11,41 +11,54 @@
 //  Define a class
 public class Arithmetic{
     
+
+    
 //  Add main method
     public static void main(String[]args){
     
-//  Input Variables
+        //  Input Variables
+        
+        //  Pennsylvania Sales Tax
+            double taxPercent=0.06;
+        //  Numbers of pairs of socks
+            int nSocks=3;
+        //  Cost per pair of socks
+            double sockCost=2.58;
+        //  Total Sock Cost
+            double totalSockCost=((nSocks*sockCost)+(nSocks*sockCost)*(taxPercent));
+        //  Number of Drinking Glasses
+            int nGlasses=6;
+        //  Cost Per Glass
+            double glassCost=2.29;
+        //  Total Glasses Cost
+            double totalglassCost=((nGlasses*glassCost)+(nGlasses*glassCost)*(taxPercent));
+        //  Number of Boxes of Envelopes
+            int nEnvelopes=1;
+        //  Cost per box of Envelopes
+            double envelopeCost=3.25;
+        //  Total Envelope Box Cost
+            double totalenvelopeCost=((nEnvelopes*envelopeCost)+(nEnvelopes*envelopeCost)*(taxPercent));
+            
+          
+            
 
-//  Pennsylvania Sales Tax
-    double taxPercent=0.06;
-//  Numbers of pairs of socks
-    int nSocks=3;
-//  Cost per pair of socks
-    double sockCost=2.58;
-//  Total Sock Cost
-    double totalSockCost=((nSocks*sockCost)+(nSocks*sockCost)*(taxPercent));
-//  Number of Drinking Glasses
-    int nGlasses=6;
-//  Cost Per Glass
-    double glassCost=2.29;
-//  Total Glasses Cost
-    double totalglassCost=((nGlasses*glassCost)+(nGlasses*glassCost)*(taxPercent));
-//  Number of Boxes of Envelopes
-    int nEnvelopes=1;
-//  Cost per box of Envelopes
-    double envelopeCost=3.25;
-//  Total Envelope Box Cost
-    double totalenvelopeCost=((nEnvelopes*envelopeCost)+(nEnvelopes*envelopeCost)*(taxPercent));
-    
-    
-//  Print Receipt Results
-    System.out.println("Socks: 3 = "+totalSockCost+"");
-    System.out.println("Tax on Socks:"+totalSockCost*taxPercent+"");
-    System.out.println("Glasses: 6 = "+(totalglassCost+""));
-    System.out.println("Tax on Glasses:"+(totalglassCost*taxPercent+""));
-    System.out.println("Envelopes: 1 = "+totalenvelopeCost+"");
-    System.out.println("Tax on Envelopes:"+(totalenvelopeCost*taxPercent+""));
-    System.out.println("Total:"+(((totalSockCost+(totalSockCost*taxPercent))+(totalglassCost+(totalglassCost*taxPercent))+(totalenvelopeCost+(totalenvelopeCost*taxPercent))+"")));
-    
-    }
-}
+
+            
+            //  Print Receipt Results
+                System.out.printf("Socks: 3 = "+totalSockCost+"");
+                System.out.println();
+                System.out.printf("Tax on Socks: "+totalSockCost*taxPercent+"");
+                System.out.println();
+                System.out.printf("Glasses: 6 = "+(totalglassCost+""));
+                System.out.println();
+                System.out.printf("Tax on Glasses: "+(totalglassCost*taxPercent+""));
+                System.out.println();
+                System.out.printf("Envelopes: 1 = "+totalenvelopeCost+"");
+                System.out.println();
+                System.out.printf("Tax on Envelopes: "+(totalenvelopeCost*taxPercent+""));
+                System.out.println();
+                System.out.printf("Total: $ "+(((totalSockCost+(totalSockCost*taxPercent))+(totalglassCost+(totalglassCost*taxPercent))+(totalenvelopeCost+(totalenvelopeCost*taxPercent))+"")));
+                System.out.println();
+                
+                }
+            }
