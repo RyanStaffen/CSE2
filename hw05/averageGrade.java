ae07377020695d3468c308a5cc82059b1734b70f
ryanstaffen <rns216@lehigh.edu>
2015-09-28 22:30:00 +0000

    updated averageGrade

---
+++ hw05/averageGrade.java
@@ -0,0 +1,64 @@
+//Ryan Staffen
+//CSE 002
+//9.28.2015
+//HW 05 averageGrade
+
+//This program uses while loops and if-else statements to 
+//take input from the user of exam scores and then average the 
+//scores entered upon a user input of "999" to terminate the loop
+
+import java.util.Scanner;
+
+    public class averageGrade{
+        public static void main(String[] args){
+            
+        Scanner scan = new Scanner(System.in);
+        
+        //Print input statment and store test scores
+        
+        int testscore;
+        int count = 0;
+        int combinedscore = 0;
+        
+        System.out.print("Please enter the test scores on a grade scale of 0 to 100: ");
+        
+        testscore = scan.nextInt();
+        
+                while (testscore < 0 || testscore > 100){
+                    
+                    System.out.print("That is not a valid test score, please enter a valid score: ");
+                    
+                    testscore = scan.nextInt();
+                    
+                    break;
+                }
+                
+                while (testscore > 0 && testscore <= 100){
+                    
+                    if(testscore != 999){
+                        
+                        combinedscore += testscore;
+                        count++;
+                        
+                        System.out.print("Please enter the next score: ");
+                        testscore = scan.nextInt();
+                        
+                        
+                    }
+                    
+                    if(testscore == 999){
+                        int average = combinedscore/count;
+                        
+                        System.out.println("The average of the scores was: "+average);
+                    }
+                }
+        
+            
+            
+            
+            
+            
+            
+            
+        }
+    }
