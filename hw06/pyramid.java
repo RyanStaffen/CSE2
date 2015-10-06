b30b95f5ac67c442b4657e2ddf18da839112baf4
ryanstaffen <rns216@lehigh.edu>
2015-10-06 23:53:30 +0000

    updated pyramid

---
+++ hw06/pyramid.java
@@ -0,0 +1,48 @@
+//Ryan Staffen
+//CSE 002
+//10-6-2015
+//HW 06
+
+//This program accepts an integer user input and then outputs a pyramid design
+//sized based off the input using a combination of loops
+
+import java.util.Scanner;
+
+public class pyramid{
+    public static void main(String[] args){
+
+    //declare scanner
+    Scanner scan1 = new Scanner(System.in);
+    
+    //Print input statement
+    System.out.print("Please enter the desired pyramid size: ");
+    
+    int size = scan1.nextInt();
+    
+    
+    for (int rows = 1 ; rows <= size ; rows++){
+        for (int spaces = 1 ; spaces <= size - rows ; spaces++){
+            System.out.print(" ");
+            
+            for (int stars = 1; stars <= 2*rows-1; stars++){
+                System.out.print("*");
+            }
+            
+        }
+        
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
